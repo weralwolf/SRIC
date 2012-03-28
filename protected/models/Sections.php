9111e1809d7f5431a849819b4ca6b5c278ed0676
Anatolii Koval <weralwolf@gmail.com>
2012-03-28 17:29:05 +0300

    Participants form internationalization

--- protected/models/Sections.php
+++ protected/models/Sections.php
@@ -9,98 +9,91 @@
  * @property string $title
  * @property string $subtitle
  */
-class Sections extends CActiveRecord
-{
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @return Sections the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+class Sections extends CActiveRecord {
+    /**
+     * Returns the static model of the specified AR class.
+     * @return Sections the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'sections';
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'sections';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('conferences_id, title, subtitle', 'required'),
-			array('conferences_id', 'length', 'max'=>10),
-			array('title', 'length', 'max'=>255),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, conferences_id, title, subtitle', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('conferences_id, title, subtitle', 'required'),
+            array('conferences_id', 'length', 'max' => 10),
+            array('title', 'length', 'max' => 255),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, conferences_id, title, subtitle', 'safe', 'on' => 'search'),
+        );
+    }
 
-	public function dropDown() {
-	    $rows = $this->findAll(/*Should be condition for exact conference*/);
-	    $dropDown = array();
-	    foreach($rows as $row) {
-	        $dropDown[$row->id] = $row->title;
-	    }
-	    return $dropDown;
-	}
+    public function dropDown() {
+        $rows = $this->findAll( /*Should be condition for exact conference*/);
+        $dropDown = array();
+        foreach ($rows as $row) {
+            $dropDown[$row->id] = $row->title;
+        }
+        return $dropDown;
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'reports' => array(self::HAS_MANY, 'Reports', 'sections_id'),
-			'conferences' => array(self::BELONGS_TO, 'Conferences', 'conferences_id'),
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'reports' => array(self::HAS_MANY, 'Reports', 'sections_id'),
+            'conferences' => array(self::BELONGS_TO, 'Conferences', 'conferences_id'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'Id',
-			'conferences_id' => 'Conferences',
-			'title' => 'Title',
-			'subtitle' => 'Subtitle',
-		);
-	}
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id' => 'Id',
+            'conferences_id' => 'Conferences',
+            'title' => 'Title',
+            'subtitle' => 'Subtitle',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search() {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria = new CDbCriteria;
 
-		$criteria->compare('id',$this->id,true);
+        $criteria->compare('id', $this->id, true);
 
-		$criteria->compare('conferences_id',$this->conferences_id,true);
+        $criteria->compare('conferences_id', $this->conferences_id, true);
 
-		$criteria->compare('title',$this->title,true);
+        $criteria->compare('title', $this->title, true);
 
-		$criteria->compare('subtitle',$this->subtitle,true);
+        $criteria->compare('subtitle', $this->subtitle, true);
 
-		return new CActiveDataProvider('Sections', array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider('Sections', array(
+            'criteria' => $criteria,
+        ));
+    }
 }
--- protected/views/participants/_form.php
+++ protected/views/participants/_form.php
@@ -2,10 +2,11 @@
 
 <?php
 $form = $this->beginWidget('CActiveForm', array(
-	'id'=>'participants-form',
-	'enableAjaxValidation'=>false,
+    'id' => 'participants-form',
+    'enableAjaxValidation' => false,
 ));
 
+$m = Yii::app()->messages;
 ?>
 	<!--p class="note">Fields with <span class="required">*</span> are required.</p-->
 	<?php echo $form->errorSummary($model); ?>
@@ -19,166 +20,174 @@ $form = $this->beginWidget('CActiveForm', array(
 	-->
 
 	<div class="registrationBlock">
-		<p class="title">Персональные данные</p>
-		<p class="note">Украинские участники должны заполнить эту группу полей на украинском языке, а иностранные участники произвольно на русском или на английском языке.</p>
+		<p class="title"><?php echo $m->translate('Participants', 'personal_data_title'); ?></p>
+		<p class="note"><?php echo $m->translate('Participants', 'personal_data_note'); ?></p>
     	<div class="row" style="float: left; width: 50%;">
-    		<?php echo $form->labelEx($model,'contries_id'); ?>
+    		<?php echo $form->labelEx($model, 'contries_id'); ?>
     		<?php //echo $form->textField($model,'contries_id',array('size'=>10,'maxlength'=>10)); ?>
     		<?php $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
-    			//'model'=>$model,
-    			//'attribute'=>'id',
-    			'id' => 'countryName',
-    			'name' => 'countryName',
-    			'source' => $this->createUrl('request/suggestCountry'),
-    		    'value' => $model->country,
-    			'htmlOptions'=>array(
-    //				'size'=>'40'
-                    'style'=>"width: 95%;"
-    			),
-    		));?>
-    		<?php echo $form->error($model,'contries_id'); ?>
+    //'model'=>$model,
+    //'attribute'=>'id',
+    'id' => 'countryName',
+    'name' => 'countryName',
+    'source' => $this->createUrl('request/suggestCountry'),
+    'value' => $model->country,
+    'htmlOptions' => array(
+        //				'size'=>'40'
+        'style' => "width: 95%;"
+    ),
+));
+?>
+    		<?php echo $form->error($model, 'contries_id'); ?>
     	</div>
     
     	<div class="row" style="float: right; width: 50%;">
-    		<?php echo $form->labelEx($model,'cities_id'); ?>
+    		<?php echo $form->labelEx($model, 'cities_id'); ?>
     		<?php //echo $form->textField($model,'cities_id',array('size'=>10,'maxlength'=>10)); ?>
     		<?php $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
-    			//'model'=>$model,
-    			//'attribute'=>'id',
-    			'id'=>'cityName',
-    			'name'=>'cityName',
-    			'value' => $model->city,
-    			'source'=>$this->createUrl('request/suggestCity'),
-    			'htmlOptions'=>array(
-    //				'size'=>'40'
-                    'style'=>"width: 100%;"
-    			),
-    		));?>
-    		<?php echo $form->error($model,'cities_id'); ?>
+    //'model'=>$model,
+    //'attribute'=>'id',
+    'id' => 'cityName',
+    'name' => 'cityName',
+    'value' => $model->city,
+    'source' => $this->createUrl('request/suggestCity'),
+    'htmlOptions' => array(
+        //				'size'=>'40'
+        'style' => "width: 100%;"
+    ),
+));
+?>
+    		<?php echo $form->error($model, 'cities_id'); ?>
     	</div>
     	
     	<div class="row">
-    		<?php echo $form->labelEx($model,'name'); ?>
-    		<?php echo $form->textField($model,'name',array('style'=>"width: 100%;",'maxlength'=>255)); ?>
-    		<?php echo $form->error($model,'name'); ?>
+    		<?php echo $form->labelEx($model, 'name'); ?>
+    		<?php echo $form->textField($model, 'name', array('style' => "width: 100%;", 'maxlength' => 255)); ?>
+    		<?php echo $form->error($model, 'name'); ?>
     	</div>
     
     	<div class="row">
-    		<?php echo $form->labelEx($model,'second_name'); ?>
-    		<?php echo $form->textField($model,'second_name',array('style'=>"width: 100%;",'maxlength'=>255)); ?>
-    		<?php echo $form->error($model,'second_name'); ?>
+    		<?php echo $form->labelEx($model, 'second_name'); ?>
+    		<?php echo $form->textField($model, 'second_name', array('style' => "width: 100%;", 'maxlength' => 255)); ?>
+    		<?php echo $form->error($model, 'second_name'); ?>
     	</div>
     
     	<div class="row" style="width: 100%;">
-    		<?php echo $form->labelEx($model,'last_name'); ?>
-    		<?php echo $form->textField($model,'last_name',array('style'=>"width: 100%;",'maxlength'=>255)); ?>
-    		<?php echo $form->error($model,'last_name'); ?>
+    		<?php echo $form->labelEx($model, 'last_name'); ?>
+    		<?php echo $form->textField($model, 'last_name', array('style' => "width: 100%;", 'maxlength' => 255)); ?>
+    		<?php echo $form->error($model, 'last_name'); ?>
     	</div>
     
     	<div class="row" style="float: left; width: 50%;">
-    		<?php echo $form->labelEx($model,'gender'); ?>
+    		<?php echo $form->labelEx($model, 'gender'); ?>
     		<?php echo $form->dropDownList($model, 'gender', array("0" => "Female", "1" => "Male")); ?>
-    		<?php echo $form->error($model,'gender'); ?>
+    		<?php echo $form->error($model, 'gender'); ?>
     	</div>
     
     	<div class="row" style="float: right; width: 50%;">
-    		<?php echo $form->labelEx($model,'birthdate'); ?>
+    		<?php echo $form->labelEx($model, 'birthdate'); ?>
     		<?php //echo $form->textField($model,'birthdate'); ?>
     		<?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
-    		    'name'=>'birthdate',
-    		    // additional javascript options for the date picker plugin
-    		    'model' => $model,
-    		
-    			'attribute' => 'birthdate',
-    		
-    		    'options'=>array(
-    		        'showAnim'=>'fold',
-    		    ),
-    		
-    		    'htmlOptions'=>array(
-    		        'style'=>'height:20px;'
-    		    ),
-    		));?>
-    		<?php echo $form->error($model,'birthdate'); ?>
+    'name' => 'birthdate',
+    // additional javascript options for the date picker plugin
+    'model' => $model,
+
+    'attribute' => 'birthdate',
+
+    'options' => array(
+        'showAnim' => 'fold',
+    ),
+
+    'htmlOptions' => array(
+        'style' => 'height:20px;'
+    ),
+));
+?>
+    		<?php echo $form->error($model, 'birthdate'); ?>
     	</div>
     
     	<div class="row"  style="width: 100%;">
-    		<?php echo $form->labelEx($model,'organizations_id'); ?>
+    		<?php echo $form->labelEx($model, 'organizations_id'); ?>
     		<?php //echo $form->textField($model,'organizations_id',array('size'=>10,'maxlength'=>10)); ?>
     		<?php $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
-    			//'model'=>$model,
-    			//'attribute'=>'id',
-    			'id'=>'organizationName',
-    			'name'=>'organizationName',
-    		    'value' => $model->organization,
-    			'source'=>$this->createUrl('request/suggestOrganization'),
-    			'htmlOptions'=>array(
-    //				'size'=>'40'
-                    'style' => "width: 100%;",
-    			),
-    		));?>
-    		<?php echo $form->error($model,'organizations_id'); ?>
+    //'model'=>$model,
+    //'attribute'=>'id',
+    'id' => 'organizationName',
+    'name' => 'organizationName',
+    'value' => $model->organization,
+    'source' => $this->createUrl('request/suggestOrganization'),
+    'htmlOptions' => array(
+        //				'size'=>'40'
+        'style' => "width: 100%;",
+    ),
+));
+?>
+    		<?php echo $form->error($model, 'organizations_id'); ?>
     	</div>
     
     	<div class="row" style="width: 100%;">
-    		<?php echo $form->labelEx($model,'post'); ?>
-    		<?php echo $form->textField($model,'post',array('style' => "width: 100%;",'maxlength'=>255)); ?>
-    		<?php echo $form->error($model,'post'); ?>
+    		<?php echo $form->labelEx($model, 'post'); ?>
+    		<?php echo $form->textField($model, 'post', array('style' => "width: 100%;", 'maxlength' => 255)); ?>
+    		<?php echo $form->error($model, 'post'); ?>
     	</div>
     
     	<div class="row" style="float: left; width: 50%;">
-    		<?php echo $form->labelEx($model,'email'); ?>
-    		<?php echo $form->textField($model,'email',array('style' => "width: 95%;",'maxlength'=>255)); ?>
-    		<?php echo $form->error($model,'email'); ?>
+    		<?php echo $form->labelEx($model, 'email'); ?>
+    		<?php echo $form->textField($model, 'email', array('style' => "width: 95%;", 'maxlength' => 255)); ?>
+    		<?php echo $form->error($model, 'email'); ?>
     	</div>
     
     	<div class="row" style="float: right; width: 50%;">
-    		<?php echo $form->labelEx($model,'phone'); ?>
-    		<?php echo $form->textField($model,'phone',array('style' => "width: 100%;",'maxlength'=>255)); ?>
-    		<?php echo $form->error($model,'phone'); ?>
+    		<?php echo $form->labelEx($model, 'phone'); ?>
+    		<?php echo $form->textField($model, 'phone', array('style' => "width: 100%;", 'maxlength' => 255)); ?>
+    		<?php echo $form->error($model, 'phone'); ?>
     	</div>
 	</div>
 
 	<div class="registrationBlock">
-		<p class="title">Доклад</p>
-		<p class="note">Поля для загрузки тезисов активируются автоматически после того, как Вы выберете опцию ДОКЛАДЧИК.
-		Если вы не планируете выступать на конференции с докладом (не завсимо от того, являетесь ли Вы соавтором доклада,
-		который будет призентировать другой соавтор), укажите опцию слушатель.</p>
-		<p class="note">Один докладывающий автор может подать не более двух работ. После загрузки первых тезисов
-		автоматически активируется кнопка ДОБАВИТЬ ЕЩЁ ДОКЛАД.</p>
+		<p class="title"><?php echo $m->translate('Participants', 'report_title'); ?></p>
+		<p class="note"><?php echo $m->translate('Participants', 'report_note_p1'); ?></p>
+		<p class="note"><?php echo $m->translate('Participants', 'report_note_p2'); ?></p>
     	<div class="row" style="overflow: auto;">
-    		<?php echo $form->labelEx($model,'participation_type'); ?>
-    		<?php echo $form->radioButtonList($model, 'participation_type', array("lecturer" => "Lecturer", "listner" => "Listner"),
-    		array("style" => "float: left; margin-right: 5px;")); ?>
-    		<?php echo $form->error($model,'participation_type'); ?>
+    		<?php echo $form->labelEx($model, 'participation_type'); ?>
+    		<?php echo $form->radioButtonList($model, 'participation_type', array(
+    "lecturer" => $m->translate('Participants', 'participation_type_lecturer'),
+    "listner" => $m->translate('Participants', 'participation_type_listner'),
+), array("style" => "float: left; margin-right: 5px;"));
+?>
+    		<?php echo $form->error($model, 'participation_type'); ?>
     	</div>
     
     	<div class="row" style="overflow: auto;">
-    		<?php echo $form->labelEx($model,'report_type'); ?>
-    		<?php echo $form->radioButtonList($model, 'report_type', array("plenary" => "Plenary", "sessional" => "Sessional", "poster" => "Poster"),
-    		array("style" => "float: left; margin-right: 5px;")); ?>
-    		<?php echo $form->error($model,'report_type'); ?>
+    		<?php echo $form->labelEx($model, 'report_type'); ?>
+    		<?php echo $form->radioButtonList($model, 'report_type', array(
+    "plenary" => $m->translate('Participants', 'report_type_plenary'),
+    "sessional" => $m->translate('Participants', 'report_type_sessional'),
+    "poster" => $m->translate('Participants', 'report_type_poster'),
+), array("style" => "float: left; margin-right: 5px;"));
+?>
+    		<?php echo $form->error($model, 'report_type'); ?>
     	</div>
     
     	<div class="row">
-    		<?php echo $form->labelEx($model,'sections_id'); ?>
-    		<?php echo $form->dropDownList($model,'sections_id', Sections::model()->dropDown()); ?>
-    		<?php echo $form->error($model,'sections_id'); ?>
+    		<?php echo $form->labelEx($model, 'sections_id'); ?>
+    		<?php echo $form->dropDownList($model, 'sections_id', Sections::model()->dropDown()); ?>
+    		<?php echo $form->error($model, 'sections_id'); ?>
     	</div>
 	</div>
 
 	<div class="row" style="float: left; width: 50%;">
-		<?php echo $form->labelEx($model,'accommodation_places_id'); ?>
-		<?php echo $form->dropDownList($model,'accommodation_places_id', Sections::model()->dropDown()); ?>
+		<?php echo $form->labelEx($model, 'accommodation_places_id'); ?>
+		<?php echo $form->dropDownList($model, 'accommodation_places_id', Sections::model()->dropDown()); ?>
 		<!-- ?php echo $form->textField($model,'accommodation_places_id',array('size'=>10,'maxlength'=>10)); ? -->
-		<?php echo $form->error($model,'accommodation_places_id'); ?>
+		<?php echo $form->error($model, 'accommodation_places_id'); ?>
 	</div>
 
 	<div class="row" style="float: right; width: 50%;">
-		<?php echo $form->labelEx($model,'accommodation_places_rooms_types_id'); ?>
-		<?php echo $form->dropDownList($model,'accommodation_places_rooms_types_id', Sections::model()->dropDown()); ?>
+		<?php echo $form->labelEx($model, 'accommodation_places_rooms_types_id'); ?>
+		<?php echo $form->dropDownList($model, 'accommodation_places_rooms_types_id', Sections::model()->dropDown()); ?>
 		<!-- ?php echo $form->textField($model,'accommodation_places_rooms_types_id',array('size'=>10,'maxlength'=>10)); ? -->
-		<?php echo $form->error($model,'accommodation_places_rooms_types_id'); ?>
+		<?php echo $form->error($model, 'accommodation_places_rooms_types_id'); ?>
 	</div>
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
